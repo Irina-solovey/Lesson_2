e64abc24c8d8bc0a6288708175fad1894283bb41
Irina-solovey <78273623+Irina-solovey@users.noreply.github.com>
2021-02-07 12:55:09 +0200

    Update ideal_weight.rb
    Added spaces

--- ideal_weight.rb
+++ ideal_weight.rb
@@ -1,15 +1,16 @@
 # Идеальный вес
+
 # Получим от пользователя значения имени и роста
 puts "Как Вас зовут?"
-name=gets.chomp
+name = gets.chomp
 
 puts "Какой у Вас рост?"
-height=gets.to_f
+height = gets.to_f
 
 # Вычислим значение идеального веса
-ideal_weight=(height-110)*1.15
+ideal_weight = (height - 110) * 1.15
 
-if ideal_weight<0
+if ideal_weight < 0
   puts "#{name}, Ваш вес уже оптимальный!"
 else
   puts "#{name}, Ваш идеальный вес #{ideal_weight.round(2)} кг!"
