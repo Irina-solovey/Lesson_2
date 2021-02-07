d768bc9f5972f12d06bf9b40885e397d0fcf46d2
Irina-solovey <78273623+Irina-solovey@users.noreply.github.com>
2021-02-07 13:00:04 +0200

    Update right_triangle.rb
    Added spaces

--- right_triangle.rb
+++ right_triangle.rb
@@ -2,20 +2,20 @@
 
 # Получим от пользователя значения трёх сторон треугольника
 puts "Введите значение первой стороны треугольника"
-side_1=gets.to_f
+side_1 = gets.to_f
 
 puts "Введите значение второй стороны треугольника"
-side_2=gets.to_f
+side_2 = gets.to_f
 
 puts "Введите значение третьей стороны треугольника"
-side_3=gets.to_f
+side_3 = gets.to_f
 
 # Выведем на экран определение треугольника
-if side_1==side_2 && side_2==side_3
+if side_1 == side_2 && side_2 == side_3
   puts "Треугольник равносторонний!"
-elsif (side_1==side_2 && side_2!=side_3) || (side_2==side_3 && side_3!=side_1) || (side_3==side_1 && side_1!=side_2)
+elsif (side_1 == side_2 && side_2 != side_3) || (side_2 == side_3 && side_3 != side_1) || (side_3 == side_1 && side_1 != side_2)
   puts "Треугольник равнобедренный!"
-elsif (side_1**2==side_2**2+side_3**2) || (side_2**2==side_3**2+side_1**2) || (side_3**2==side_1**2+side_2**2)
+elsif (side_1**2 == side_2**2 + side_3**2) || (side_2**2 == side_3**2 + side_1**2) || (side_3**2 == side_1**2 + side_2**2)
   puts "Треугольник прямоугольный!"
 else
   puts "Треугольник разносторонний, но не прямоугольный!"
